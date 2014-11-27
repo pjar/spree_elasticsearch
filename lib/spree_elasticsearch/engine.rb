6b6092c9155c2aaacaf9035fa7dc7bc895ed97e7
Jarek Plonski <jarek.github@gmail.com>
2014-11-27 22:13:54 +0100

    trying to set ES as searcher_class

--- lib/spree_elasticsearch/engine.rb
+++ lib/spree_elasticsearch/engine.rb
@@ -12,6 +12,8 @@ module SpreeElasticsearch
     end
 
     def self.activate
+
+      Spree::Config.searcher_class = Spree::Search::Elasticsearch
       Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
       end
