66f6aa083c4265076578dc02d0b98e2bd41ac887
Jarek Plonski <jarek.github@gmail.com>
2014-11-27 22:21:02 +0100

    reverted setting searcher class - facets are broken

--- lib/spree_elasticsearch/engine.rb
+++ lib/spree_elasticsearch/engine.rb
@@ -13,7 +13,6 @@ module SpreeElasticsearch
 
     def self.activate
 
-      Spree::Config.searcher_class = Spree::Search::Elasticsearch
       Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
       end
